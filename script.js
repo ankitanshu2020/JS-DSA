cc7331d24725ede7a04e99cae142d95138b16971
Ankitanshu Kr <ankieduhorizon@gmail.com>
2024-08-19 17:11:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let candiateName = "Bujji";
+let age = 23;
+let isGraduated = true;
+
+console.table([candiateName, age, isGraduated]);
+
+//command to run js code :- node script.js
+//-------console result is below '.'
+// ┌─────────┬─────────────────┐
+// │ (index) │ Values          │
+// ├─────────┼─────────────────┤
+// │ 0       │ 'Bujji'         │
+// │ 1       │ 23              │
+// │ 2       │ true            │
+// └─────────┴─────────────────┘
